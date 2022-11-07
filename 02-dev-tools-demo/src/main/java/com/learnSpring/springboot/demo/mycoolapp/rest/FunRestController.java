256353c9ed14b8873cf9f6e394e10a9d8c0cb374
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-07 16:43:39 +0200

    added dev tools dependency and test it with new endpoints

--- 02-dev-tools-demo/src/main/java/com/learnSpring/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 02-dev-tools-demo/src/main/java/com/learnSpring/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -8,9 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-	// expose "/"  that return "Hello World!"
+	// expose "/" that return "Hello World!"
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	// expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day";
 	}
 }
