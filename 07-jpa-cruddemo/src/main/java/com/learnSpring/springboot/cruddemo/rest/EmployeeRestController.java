a385ecebc22cc776938a4b665c4f969ac21f8140
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-16 17:07:04 +0200

    remove unnecessary imports

--- 07-jpa-cruddemo/src/main/java/com/learnSpring/springboot/cruddemo/rest/EmployeeRestController.java
+++ 07-jpa-cruddemo/src/main/java/com/learnSpring/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,8 +2,6 @@ package com.learnSpring.springboot.cruddemo.rest;
 
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
