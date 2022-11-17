b7a5b36425d31d8b5f4e90b70c7e22e0b675e4da
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-17 12:24:29 +0200

    replaced the Employee Class with one that mapped to db

--- 14-thymeleafdemo-employees-list-db/src/main/java/com/learnSpring/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 14-thymeleafdemo-employees-list-db/src/main/java/com/learnSpring/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.learnSpring.springboot.thymeleafdemo.model.Employee;
+import com.learnSpring.springboot.thymeleafdemo.entity.Employee;
 
 @Controller
 @RequestMapping("/employees")
--- 14-thymeleafdemo-employees-list-db/src/main/java/com/learnSpring/springboot/thymeleafdemo/entity/Employee.java
+++ 14-thymeleafdemo-employees-list-db/src/main/java/com/learnSpring/springboot/thymeleafdemo/entity/Employee.java
@@ -1,15 +1,40 @@
-package com.learnSpring.springboot.thymeleafdemo.model;
+package com.learnSpring.springboot.thymeleafdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "email")
 	private String email;
 	
 	public Employee() {
-		// TODO Auto-generated constructor stub
 	}
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
 
 	public Employee(int id, String firstName, String lastName, String email) {
 		this.id = id;
@@ -55,5 +80,12 @@ public class Employee {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 	
-
 }
+
+
+
+
+
+
+
+
