bb1d1291d33f28671664e83d022b58f0f25595f8
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-11-17 13:22:23 +0200

    created the form page and its' method at the controller to handle it

--- 15-thymeleafdemo-employees-add/src/main/java/com/learnSpring/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 15-thymeleafdemo-employees-add/src/main/java/com/learnSpring/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -2,10 +2,14 @@ package com.learnSpring.springboot.thymeleafdemo.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.learnSpring.springboot.thymeleafdemo.entity.Employee;
@@ -45,6 +49,16 @@ public class EmployeeController {
 		
 		
 		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		// save the employee
+		employeeService.save(employee);
+		
+		// redirect to main page
+		return "redirect:/employees/list";
 	}
 }
 
